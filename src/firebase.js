d3c026208fe4a728a06a3be9627b4fa7e65e542a
Rafael Chipitelli <you@example.comRafaelChipitelli.o.c27@gmail.com>
2025-07-24 20:13:01 +1000

    test

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY || "",
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN || "",
-  projectId: process.env.FIREBASE_PROJECT_ID || "",
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "",
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "",
-  appId: process.env.FIREBASE_APP_ID || ""
+  apiKey: "AIzaSyDTJSH0WFZIZ77tXonpePM_k7Q3nbduSY0",
+  authDomain: "bulletrush-fun.firebaseapp.com",
+  projectId: "bulletrush-fun",
+  storageBucket: "bulletrush-fun.firebasestorage.app",
+  messagingSenderId: "23163026813",
+  appId: "1:23163026813:web:e6ffd29924e2fac884e014"
 };
 
 // firebase is loaded globally by CDN script tags in index.html
